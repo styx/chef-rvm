cdcecde5a9c382ed58c11f4a8a5e6ff2cc6b9f2b
Mikhail S. Pobolovets <styx.mp@gmail.com>
2013-04-04 12:14:31 +0300

    Support for patch option.
    Signed-off-by: Mikhail S. Pobolovets <styx.mp@gmail.com>

--- recipes/install.rb
+++ recipes/install.rb
@@ -8,11 +8,15 @@ ruby_version = [].tap do |v|
   v << node[:rvm][:ruby][:patch_level] if node[:rvm][:ruby][:patch_level]
 end * '-'
 
+ruby_patchsets = if node[:rvm][:ruby][:patchsets]
+  '--patch ' + node[:rvm][:ruby][:patchsets] * ' '
+end
+
 include_recipe 'rvm::default'
 
 bash "installing #{ruby_version}" do
   user 'root'
-  code "/usr/local/rvm/bin/rvm install #{ruby_version}"
+  code "/usr/local/rvm/bin/rvm install #{ruby_version} #{ruby_patchsets}"
   not_if "/usr/local/rvm/bin/rvm list | grep #{ruby_version}"
 end
 
